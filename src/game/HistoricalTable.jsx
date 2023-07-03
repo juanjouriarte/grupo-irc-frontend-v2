1dbbf45ae8bd5e466572397dd6c80a51d031a407
juanjouriarte <juanjouriarte@uc.cl>
2023-07-03 03:12:45 -0400

    frontend para deploy

--- src/game/HistoricalTable.jsx
+++ src/game/HistoricalTable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './HistoricalTable.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { API_URL } from '../config';
+import API_URL  from '../config';
 
 const HistoricalTable = () => {
   const [games, setGames] = useState([]);
